a1f07f2b8adfb52f2bd787b9cb8d8f5072497299
jocelynthiojaya <jocelynthiojaya@gmail.com>
2020-12-10 13:52:21 +0700

    Neural network updated it works.

--- carbrain/neuralnetwork.py
+++ carbrain/neuralnetwork.py
@@ -1,27 +1,28 @@
 import numpy as np
-from scipy.stats import logistic
+#from scipy.stats import logistic
 
 class NeuralNetwork:
-    def __init__(self,x):
-        self.inputlayer = x
-        self.weight1 = np.random.randint(10, size=5)
-        self.weight2 = np.random.randint(10, size=2)
-        self.outputlayer = np.zeros(shape=(1,2))
+    def __init__(self, x):
+        self.input = x
+        self.weight1 = np.random.rand(5, 4)
+        self.weight2 = np.random.rand(4, 3)
+        self.weight3 = np.random.rand(3, 2)
+        self.output = np.zeros(shape=(1,2))
     
+    def sigmoid(self, num):
+        value = 1/(1+np.exp(-num))
+        return value
+
     def feedforward(self):
-        self.layer1 = 2 * (logistic.cdf(np.dot(self.inputlayer,self.weight1)))-1
-        self.outputlayer = 2 *(logistic.cdf(np.dot(self.layer1,self.weight2)))-1
-        return self.outputlayer
+        self.layer1 = self.sigmoid(np.dot(self.input, self.weight1))
+        self.layer2 = self.sigmoid(np.dot(self.layer1, self.weight2))
+        self.output = self.sigmoid(np.dot(self.layer2, self.weight3))
+        return self.output
     
-      
-inputvalue = np.array([-1,-1,0,1,1])
+  
+inputvalue = np.array([5,2,3,1,4])
 neural1 = NeuralNetwork(inputvalue)
-print("output layer", neural1.feedforward())
-print("random weight1 ",neural1.weight1)
-print("random weight2 ",neural1.weight2)
-
-
-
-
-
-        
+print("output layer: \n", neural1.feedforward())
+#print("random weight1: \n",neural1.weight1)
+#print("random weight2: \n ",neural1.weight2)
+#print(inputvalue.shape)
