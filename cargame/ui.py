b36df2e66b82dc1fe5821a4cf0b01643156768e7
Jason Christian H <nakamarujc@gmail.com>
2020-12-17 12:10:12 +0700

    Added buttons for the UI, later will add press functionality

--- cargame/ui.py
+++ cargame/ui.py
@@ -2,6 +2,7 @@ import arcade
 from cargame.camera import Camera
 import cargame.util as util
 from cargame.globals import conf
+from time import time
 
 Y_UI_CENTER = 75
 
@@ -12,12 +13,53 @@ TEXT_WIDTH = 180
 TEXT_HEIGHT = 85
 TEXT_MARGIN = 5
 
+class Button():
+
+    width = 50
+    height = 70
+
+    def __init__(self, text, x, y, color, icon=None, variety=0):
+        """ Create a new button with or without a sprite.
+        :text: display string text
+        :x: x coordinates relative to the viewport
+        :y: y coordinates relative to the viewport
+        :color: Button color
+        :icon: path to file of the icon of the button
+        :variety: 0-1, the variety of the button """
+        # Resize the sprite to 64x64
+        self.sprite = arcade.Sprite(icon, image_width=32, image_height=32) if icon!=None else None
+        self.set_coords(x - Button.width/2,
+        y - Button.height/2)
+
+        self.text = text
+        self.color = color
+
+    def set_coords(self, x, y):
+        self.x = x
+        self.y = y
+        self.scenter_x = x + Button.width/2
+        self.scenter_y = y + Button.height - 16 - 9
+
+    def on_draw(self, camx, camy):
+        """ Needs the camx and camy coordinates to draw it relatively """
+        arcade.draw_rectangle_filled(camx + self.x + Button.width/2, camy + self.y + Button.height/2, Button.width, Button.height, self.color)
+        arcade.draw_text(self.text, camx + self.x, camy + self.y + 3, (255, 255, 255), 10, Button.width, "center")
+        self.sprite.center_x = camx + self.scenter_x
+        self.sprite.center_y = camy + self.scenter_y
+        self.sprite.draw()
+
 class GameUI():
 
     def __init__(self, camera):
         """ Inits the UI using the camera as the reference """
         self.cam: Camera = camera
         self.ui_text = ""
+
+        # The buttons inside the ui
+        self.buttons = [
+            Button("Play", conf["screen_width"]/2 - UI_WIDTH/2 + 50, Y_UI_CENTER, (245, 71, 71), "./cargame/sprites/play.png"),
+            Button("Pause", conf["screen_width"]/2 - UI_WIDTH/2 + 120, Y_UI_CENTER, (245, 71, 71), "./cargame/sprites/pause.png")
+        ]
 
     def set_text(self, text):
         """ Sets the new UI Text """
@@ -28,12 +70,13 @@ class GameUI():
         self.ui_text += text
 
     def on_draw(self):
-
+        
+        # start = time()
         # Only draws when the camera has 1x magnification.
         camx, _, camy, _ = arcade.get_viewport()
         if self.cam.zoom == 1:
             # Base UI Rectangle
-            util.draw_rectangle_rounded(camx + conf["screen_width"]/2, camy + Y_UI_CENTER, UI_WIDTH, UI_HEIGHT, 15, (230, 230, 230))
+            util.draw_rectangle_rounded(camx + conf["screen_width"]/2, camy + Y_UI_CENTER, UI_WIDTH, UI_HEIGHT, 15, (240, 240, 240))
             
             # Draw the text container and text
             text_rect_x = camx + conf["screen_width"]/2 + UI_WIDTH/2 - TEXT_WIDTH/2 - 10
@@ -42,5 +85,12 @@ class GameUI():
             arcade.draw_rectangle_filled(text_rect_x, text_rect_y, TEXT_WIDTH, TEXT_HEIGHT, (255, 255, 255))
 
             arcade.draw_text(self.ui_text, text_rect_x - TEXT_WIDTH/2 + TEXT_MARGIN, text_rect_y - TEXT_HEIGHT/2 + TEXT_MARGIN, (10, 10, 10), 12, TEXT_WIDTH - TEXT_MARGIN*2, font_name="courbd")
+
+            # Draw the buttons
+            for button in self.buttons:
+                button.on_draw(camx, camy)
+            
         else:
             arcade.draw_text("Press ` to reset zoom.", camx + 10, camy + 10, (10, 10, 10), 12, font_name="courbd")
+
+        # print("gui_draw_time: {}ms".format(round((time() - start) * 1000, 2)))
