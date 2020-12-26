058716c2ed8d208f2564b3aed2d3d12f8ae6dfeb
christynatalia <57931555+christynatalia@users.noreply.github.com>
2020-12-26 13:44:06 +0700

    fixed the crossover functions.

--- carbrain/geneticAlgorithm.py
+++ carbrain/geneticAlgorithm.py
@@ -7,19 +7,55 @@ class geneticAlgorithm:
         self.parents2 = []
         self.child1 = []
         self.child2 = []
-        
+  
     def crossover(self):
         if len(self.parents1) == len(self.parents2):
             #check the size of the parents. 
             #get the size of parents 
             sizeParents = len(parents1)
-            #generate random number to determine the bound for crossover point 
-            crossover_point = random.randint(1,(sizeParents - 1))
-            self.child1 = np.concatenate([parents1[0:crossover_point] , parents2[crossover_point:]])
-            self.child2 = np.concatenate([parents2[0:crossover_point] , parents1[crossover_point:]])
-            
-     #def calcfitness(self):
-         #fitness = 0
+
+            #generate random numbers to slice the parents into several parts.
+            slice_parents = random.randint(2,sizeParents-1)
+
+            #determine the crossover point. Generate random number to determine the bound for crossover point 
+            crossover_array = np.array([random.randint(1,(sizeParents-1)) for _ in range(slice_parents)])
+            crossover_array.sort()
+
+            #remove duplicate numbers
+            crossover_array = list(dict.fromkeys(crossover_array))
+            #count the number of slices again. 
+            slice_parents = len(crossover_array)
+
+            #do the crossover
+            for i in range(0,slice_parents):
+                bounds_top = crossover_array[i]
+                #print(bounds_top)
+                bounds_low = crossover_array[i-1]
+                if len(self.child1) == 0 :
+                    self.child1 = np.concatenate([self.child1, parents1[0:bounds_top]])
+                    self.child2 = np.concatenate([self.child2, parents2[0:bounds_top]])
+                    flag_parents = 1 
+                    # if the flag is 1, it will take the value from parents 2 for child 1 and parents 1 for child 2
+                elif flag_parents == 1:
+                    self.child1 = np.concatenate([self.child1,parents2[bounds_low:bounds_top]])
+                    self.child2 = np.concatenate([self.child2, parents1[bounds_low:bounds_top]])
+                    flag_parents = 0
+                    # if the flag is 0, it will take the value from parents 1 for child 1 and parents 2 for child 2
+                elif flag_parents == 0 and len(self.child1) is not 0:
+                    self.child1 = np.concatenate([self.child1,parents1[bounds_low:bounds_top]])
+                    self.child2 = np.concatenate([self.child2,parents2[bounds_low:bounds_top]])
+                    flag_parents = 1
+
+            #ini buat yg belakangnya.
+            if flag_parents == 0:
+                self.child1 = np.concatenate([self.child1,parents1[bounds_top:]])
+                self.child2 = np.concatenate([self.child2,parents2[bounds_top:]])
+
+            elif flag_parents == 1:
+                self.child1 = np.concatenate([self.child1,parents2[bounds_top:]])
+                self.child2 = np.concatenate([self.child2,parents1[bounds_top:]])
+                
+        return "child1", self.child1, "child2", self.child2
 
     def mutatiton(self):
         self.crossover()
@@ -31,8 +67,8 @@ class geneticAlgorithm:
     
 parents1 = np.array([random.random() for _ in range(38)])
 parents2 = np.array([random.random() for _ in range(38)])
-print("parents1 = " , parents1)
-print("parents2 = " , parents2)
+#print("parents1 = " , parents1)
+#print("parents2 = " , parents2)
 g1 = geneticAlgorithm(parents1,parents2)
 print(g1.mutatiton())
         
